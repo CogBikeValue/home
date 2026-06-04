55f739df96ef680437969ea13000d097882c7b3a
CogBikeValue <creasepeas@gmail.com>
2026-06-04 12:35:26 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,42 +4,42 @@ const pages = [
     title: "Bike Collection",
     description: "Browse every bike available in COG.",
     link: "https://cogbikevalue.github.io/collection/",
-    image: "https://tr.rbxcdn.com/180DAY-4ee0c4013d91da788f51fbc2819b3528/768/432/Image/Webp/noFilter"
+    image: "https://cogbikevalue.github.io/home/images/adwawdadwadw.png"
 },
 
 {
     title: "Bike Values",
     description: "Trading values and market information.",
     link: "https://cogbikevalue.github.io/bikes/",
-    image: "https://tr.rbxcdn.com/180DAY-6978db8db6fb5670515df9a620a480bc/768/432/Image/Webp/noFilter"
+    image: "https://cogbikevalue.github.io/home/images/awdawdadwadw.png"
 },
 
 {
     title: "COG",
     description: "The main Roblox experience.",
     link: "https://www.roblox.com/games/100550219954127/Cog",
-    image: "https://tr.rbxcdn.com/180DAY-985744c74d17867110e860601da1eee8/768/432/Image/Webp/noFilter"
+    image: "https://cogbikevalue.github.io/home/images/2602806146_100550219954127_1780547337326.png"
 },
 
 {
     title: "Highway Loop",
     description: "Long roads and cruising.",
     link: "https://www.roblox.com/games/97656354500235/Highway-Loop",
-    image: "https://tr.rbxcdn.com/180DAY-1bdfd22ea9cf1c3b3717e56adca18377/768/432/Image/Webp/noFilter"
+    image: "https://cogbikevalue.github.io/home/images/2602806146_97656354500235_1780547142012.png"
 },
 
 {
     title: "Downhill Touge",
     description: "Technical downhill riding.",
     link: "https://www.roblox.com/games/81716287708579/Cog-Touge-1",
-    image: "https://tr.rbxcdn.com/180DAY-9d19199eeca6759f8dd746127d651bd1/768/432/Image/Webp/noFilter"
+    image: "https://cogbikevalue.github.io/home/images/2602806146_81716287708579_1780547244887.png"
 },
 
 {
     title: "Trade Hub",
     description: "Meet traders and exchange bikes.",
     link: "https://www.roblox.com/games/119484254343449/Trade-Hub",
-    image: "https://tr.rbxcdn.com/180DAY-103aa322b41ccafc77d46a44a413b5a8/768/432/Image/Webp/noFilter"
+    image: "https://cogbikevalue.github.io/home/images/2602806146_119484254343449_1780547015110.png"
 },
 
 {
